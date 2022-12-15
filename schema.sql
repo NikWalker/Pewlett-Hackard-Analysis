7ce486bba54a57f1434499c3e15f1ae24e975472
Nik Walker <ketchupkid17@gmail.com>
2022-12-15 13:21:02 -0500

    created retirement info table and saved to csv, updated ERD png with all keys, updated query schema

--- schema.sql
+++ schema.sql
@@ -42,4 +42,49 @@ CREATE TABLE titles (emp_no INT NOT NULL,
 					from_date DATE NOT NULL,
 					to_date DATE NOT NULL,
 					FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-					PRIMARY KEY (emp_no));	
+					PRIMARY KEY (emp_no,title, from_date));	
+
+--7.3.1
+select * FROM titles;	
+
+--Retiring age employees 1952-1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+--1952
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+--1953
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+--1954
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+--1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+--Retirement elegibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Count the queries
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--creating table from query parameters
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;				
